0513aa13b4b72075a280d091c72ceeffdc77fb28
nichibi001 <AppCircle@nichibi01.local>
2017-08-03 22:23:20 +0900

    枠組み編集

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
-$(function(){
-	$('#fullpage').fullpage();
+$(function()
+{
+	/**
+	*	tab切り替え（header用）
+	**/
+	$('.tab li').click(
+		function()
+		{
+			//.index()を使いクリックされたタブが何番目かを調べ、
+			//indexという変数に代入します。
+			var index = $('.tab li').index(this);
+
+			//コンテンツを一度すべて非表示にし、	
+			$('.middleBox div').css('display','none');
+
+			//クリックされたタブと同じ順番のコンテンツを表示します。
+			$('.middleBox div').eq(index).css('display','block');
+
+			//一度タブについているクラスselect,activeを消し、
+			$('.tab li').removeClass('select');
+			$('.tab li').removeClass('active');
+
+			//クリックされたタブのみにクラスselect,activeをつけます。
+			$(this).addClass('select')
+			$(this).addClass('active')
+		}
+	);
+	
+	/**
+	*	tab切り替え（gnavi用）
+	**/
+	$('.tab div').click(
+		function()
+		{
+			//.index()を使いクリックされたタブが何番目かを調べ、
+			//indexという変数に代入します。
+			var index = $('.tab div').index(this);
+
+			//コンテンツを一度すべて非表示にし、	
+			$('.middleBox div').css('display','none');
+
+			//クリックされたタブと同じ順番のコンテンツを表示します。
+			$('.middleBox div').eq(index).css('display','block');
+
+			//一度タブについているクラスselectを消し、
+			$('.tab div').removeClass('select');
+
+			//クリックされたタブのみにクラスselectをつけます。
+			$(this).addClass('select')
+		}
+	);
 });
