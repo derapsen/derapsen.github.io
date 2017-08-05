6ee43de493bf059cccf296b31a2134781667afed
nichibi001 <t.kodera.nichibi@gmail.com>
2017-08-05 13:10:40 +0900

    コンテンツをpanelに変更

--- js/main.js
+++ js/main.js
@@ -1,59 +1,55 @@
-$(function()
-{
-	$(document).ready(
-		function()
-		{
-			$(".img_slide").imgLiquid();
-		}
-	);
-	
-	/**
-	*	tab切り替え（header用）
-	**/
-	$('.tab li').click(
-		function()
-		{
-			//.index()を使いクリックされたタブが何番目かを調べ、
-			//indexという変数に代入します。
-			var index = $('.tab li').index(this);
+/**
+*	tab切り替え（header用）
+**/
+$('.tab li').click(
+	function()
+	{
+		//.index()を使いクリックされたタブが何番目かを調べ、
+		//indexという変数に代入します。
+		var index = $('.tab li').index(this);
 
-			//コンテンツを一度すべて非表示にし、	
-			$('.middleBox div').css('display','none');
+		//コンテンツを一度すべて非表示にし、	
+		$('.middleBox .contents').css('display','none');
 
-			//クリックされたタブと同じ順番のコンテンツを表示します。
-			$('.middleBox div').eq(index).css('display','block');
+		//クリックされたタブと同じ順番のコンテンツを表示します。
+		$('.middleBox .contents').eq(index).css('display','block');
 
-			//一度タブについているクラスselect,activeを消し、
-			$('.tab li').removeClass('select');
-			$('.tab li').removeClass('active');
+		//一度タブについているクラスselect,activeを消し、
+		$('.tab li').removeClass('select');
+		$('.tab li').removeClass('active');
 
-			//クリックされたタブのみにクラスselect,activeをつけます。
-			$(this).addClass('select')
-			$(this).addClass('active')
-		}
-	);
-	
-	/**
-	*	tab切り替え（gnavi用）
-	**/
-	$('.tab div').click(
-		function()
-		{
-			//.index()を使いクリックされたタブが何番目かを調べ、
-			//indexという変数に代入します。
-			var index = $('.tab div').index(this);
+		//クリックされたタブのみにクラスselect,activeをつけます。
+		$(this).addClass('select')
+		$(this).addClass('active')
+	}
+);
 
-			//コンテンツを一度すべて非表示にし、	
-			$('.middleBox div').css('display','none');
+/**
+*	tab切り替え（gnavi用）
+**/
+$('.tab div').click(
+	function()
+	{
+		//.index()を使いクリックされたタブが何番目かを調べ、
+		//indexという変数に代入します。
+		var index = $('.tab div').index(this);
 
-			//クリックされたタブと同じ順番のコンテンツを表示します。
-			$('.middleBox div').eq(index).css('display','block');
+		//コンテンツを一度すべて非表示にし、	
+		$('.middleBox .contents').css('display','none');
 
-			//一度タブについているクラスselectを消し、
-			$('.tab div').removeClass('select');
+		//クリックされたタブと同じ順番のコンテンツを表示します。
+		$('.middleBox .contents').eq(index).css('display','block');
+		
+//		$('.panel').css('border-style','none');
 
-			//クリックされたタブのみにクラスselectをつけます。
-			$(this).addClass('select')
-		}
-	);
-});
+		//一度タブについているクラスselectを消し、
+		$('.tab div').removeClass('select');
+
+		//クリックされたタブのみにクラスselectをつけます。
+		$(this).addClass('select')
+	}
+);
+
+/**
+*	.jsの設定
+**/
